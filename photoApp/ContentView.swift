c585f0b2edece83c0ca2bec9916922bbfc8eafb0
mojens <moha135h@stud.kea.dk>
2023-02-28 08:31:23 +0100

    first push

---
+++ photoApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  photoApp
+//
+//  Created by Mohammad Murtada on 28/02/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+          
+            
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ photoApp/photoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  photoAppApp.swift
+//  photoApp
+//
+//  Created by Mohammad Murtada on 28/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct photoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
